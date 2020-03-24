44726be4f001b8aba6552413938a85bdd369e1d8
rbroc <rbrrcc@gmail.com>
2020-03-24 12:52:18 +0100

    mark df with is_yamnet and add json with top level labels defined in yamnet

--- audioset_utils.py
+++ audioset_utils.py
@@ -27,9 +27,12 @@ first_level_id = j_df['id'][j_df['levels'] == '1'].tolist()
 j_df['levels'][j_df['parent_ids'].isin(first_level_id)] = '2'
 
 # Store first and second level ids
-first_level_labels = j_df['name'][j_df['id'].isin(first_level_id)]
-second_level_labels = j_df['name'][j_df['parent_ids'].isin(first_level_id)]
-top_levels_dict = {'first_level': first_level_labels.tolist(), 'second_level': second_level_labels.tolist()}
+first_level_labels = j_df['name'][j_df['id'].isin(first_level_id)].tolist()
+second_level_labels = []
+for i,r in j_df.iterrows():
+    if any(l in first_level_id for l in r['parent_ids'].split()):
+        second_level_labels.append(r['name'])
+top_levels_dict = {'first_level': first_level_labels, 'second_level': second_level_labels}
 json.dump(top_levels_dict, open('top_level_labels.json',"w"))
 
 # Add further levels
@@ -69,7 +72,19 @@ for index, row in j_df.iterrows():
 for col in ['parent_ids', 'levels', 'levels_parent_ids']:
     j_df[col] = j_df[col].apply(lambda x: x.split())
 
+# Read in yamnet labels and add is_yamnet field
+yam_lab = pd.read_csv('yamnet_class_map.csv', sep='\t')
+j_df['is_yamnet'] = j_df['name'].isin(yam_lab['display_name'])
+
 # Store both as df and as dict
 j_df.to_csv('ontology_df_with_levels.txt', sep='\t')
 j_as_dict = [j_df.to_dict(orient='records')]
 json.dump(j_as_dict, open('ontology_with_levels.json',"w"))
+
+# Also store a version of top_level labels in yamnet
+first_level_yamnet = [f for f in first_level_labels if j_df['is_yamnet'][j_df['name'] == f].bool() == True]
+second_level_yamnet = [s for s in second_level_labels if j_df['is_yamnet'][j_df['name'] == s].bool() == True]
+
+top_levels_dict_yamnet = {'first_level': first_level_yamnet,
+                          'second_level': second_level_yamnet}
+json.dump(top_levels_dict, open('yamnet_top_level_labels.json',"w"))
